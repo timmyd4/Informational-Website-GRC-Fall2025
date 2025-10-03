a5e294d1d6ee436556520f7df0b87642fe9046e5
timmyd4 <timmywilliams4665@gmail.com>
2025-10-03 14:42:15 -0700

    Finished - hopefully img works

--- Header.jsx
+++ Header.jsx
@@ -1,8 +1,9 @@
+import switches from "./src/assets/Switches.png";
 export default function App()
 {
     return(<div>
         <div className="img-div">
-            <img className="switches-img" src="/Switches.png" alt="image of mechanical keyboard switches"/>
+            <img className="switches-img" src={switches} alt="image of mechanical keyboard switches"/>
         </div>
         <h1>Mechanical Keyboards </h1>
     </div>);
