32d420442373c9e6805ad5d8b0d4dc1897eee450
timmyd4 <timmywilliams4665@gmail.com>
2025-10-03 14:37:49 -0700

    save

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Informational-Website/",   // 👈 add this line
+  base: "/Informational-Website-GRC-Fall2025/",   // 👈 add this line
 })
