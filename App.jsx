4d69cc8196d49e83289cd654ebef2ed792a8f619
timmyd4 <timmywilliams4665@gmail.com>
2025-10-03 14:27:22 -0700

    finished for now

--- App.jsx
+++ App.jsx
@@ -6,6 +6,7 @@ export default function App()
     return(<div>
         <Header/>
         <Main/>
+        <br></br> 
         <Footer/>
         </div>);
 }
--- Main.jsx
+++ Main.jsx
@@ -2,6 +2,7 @@ export default function Main()
 {
     return(
         <article>
+
             <p> Mechanical keyboards are exactly like normal keyboards, except for one thing.</p>
             <p> <strong> The <em>feeling</em> you receive when pressing a key. </strong></p>
             <h3> Mechanical keyboards consist of 3 main things such as: </h3>
@@ -10,6 +11,19 @@ export default function Main()
                 <li>Switches</li>
                 <li>PCB(Printed Circut Board)</li>
             </ul>
+
+            <div className="page-text">
+
+            Many individuals like to build, customize, and create their own mechanical keyboards. <br></br>
+            Mechanical keyboards are very customizeable, from switches, to keycaps, to even the entire size of your board. <br></br>
+            Mechanical keyboards have been used for decades within the Computer Professional world. Used on early IBM machines to caculators, even some cash registers have Mechanical keyboard switches. <br></br>
+            Mechanical keyboards require and utilze soldering tools to be made, usually using diodes, along with perferrable LEDs. <br></br>
+            Diodes provide authenticity towards the key push, making sure of no "ghost presses" or "double clicks". Diodes are important for registering a single keypush. <br></br>
+            LEDs are usually purely for Looks and customization, but sometimes, they can be used for checking if the battery is low on a Wireless or Bluetooth keyboard, or also for checking if Caps lock is on and etc...
+
+
+            </div>
+
         </article>
     );
 }
